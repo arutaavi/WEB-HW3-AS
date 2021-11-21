b164ce55a95368541db406434412a09c271da6b6
RennoSepp <renno.sepp@gmail.com>
2021-11-21 18:41:02 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Posts from "../views/Posts.vue";
+import Login from "../views/Login.vue";
+import Contact from "../views/Contact.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+const routes = [{
+        path: "/posts",
+        name: "posts",
+        component: Posts
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login
+    },
+    {
+        path: "/contact",
+        name: "contact",
+        component: Contact
+    },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+<div class="flex-containeer">
+    <div>
+        <img src="src/images/NoteIt.png" alt="NoteIt">
+        <div class="helper">
+            <img src="src/images/NoteIt.png" alt="NoteIt">
+        </div>
+        <h1>Welcome to postIt!</h1>
+        <p><a href='index.html'>Create an Account</a></p>
+        <p>Or</p>
+        <p>Please Log In</p>
+        <form>
+            <input type="text" placeholder="Email"><br>
+            <input type="password" placeholder="Password"><br>
+            <input type="button" value="Log In" id="button" onclick="location.href = 'index.html'">
+        </form>
+        <p><a href='index.html'>Forgot password?</a></p>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+.flex-containeer {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
+.flex-containeer>div {
+    padding: 20px;
+    margin-top: 15px;
+    margin-bottom: 40px;
+    background-color: ghostwhite;
+    display: block;
+    text-align: center;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 15px;
+    width: 500px;
+}
+
+div img {
+    height: 100px;
+    float: left;
+    width: 100px;
+    text-align: left;
+}
+
+div .helper {
+    float: right;
+    width: 100px;
+    text-align: right;
+}
+
+div p {
+    font-size: 18px;
+}
+
+input {
+    width: 200px;
+    padding: 10px 18px;
+    margin: 8px 10px;
+    display: inline-block;
+    border: 1px solid black;
+}
+
+#button {
+    background-color: #5f7de5;
+    width: 100px;
+    border-radius: 5px;
+}
+
+#button:hover {
+    background-color: royalblue;
+}
+</style>
